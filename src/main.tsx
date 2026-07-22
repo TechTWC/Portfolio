7b24decd9e200700575cf13af4e86a938639171b
TechTWC <siencetech.twc@gmail.com>
2026-07-22 22:42:42 +0800

    Mount performance and XIRR workspace

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import FxCostStandalone from './FxCostStandalone'
+import PerformanceStandalone from './PerformanceStandalone'
 import ValuationStandalone from './ValuationStandalone'
 import './styles.css'
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <ValuationStandalone />
     <FxCostStandalone />
+    <PerformanceStandalone />
   </StrictMode>,
 )
