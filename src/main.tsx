9a6f4a41b43df6de3b41068d765c91c11f5ac449
TechTWC <siencetech.twc@gmail.com>
2026-07-22 16:46:21 +0800

    Render valuation workspace portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import ValuationPortal from './ValuationPortal'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ValuationPortal />
   </StrictMode>,
 )
